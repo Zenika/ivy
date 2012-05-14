8368cde660ac8b4b6f596be8a389374ac144603c
Adrien Lecharpentier <adrien.lecharpentier@zenika.com>
2012-05-14 14:06:40 +0200

    Fix zenika copyright on cudf related classes

--- test/java/org/apache/ivy/plugins/resolver/CUDFResolverTest.java
+++ test/java/org/apache/ivy/plugins/resolver/CUDFResolverTest.java
@@ -1,3 +1,18 @@
+/*
+ *      Copyright 2012 Zenika
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.ivy.plugins.resolver;
 
 import junit.framework.TestCase;
@@ -32,7 +47,7 @@ public class CUDFResolverTest extends TestCase {
     }
 
     public void testGetDependency() throws ParseException {
-        ModuleRevisionId moduleRevisionId = ModuleRevisionId.newInstance("org/apache", "test", "1.0");
+        ModuleRevisionId moduleRevisionId = ModuleRevisionId.newInstance("org.apache", "test", "1.0");
         DependencyDescriptor dependencyDescriptor = new DefaultDependencyDescriptor(moduleRevisionId, false);
         ResolvedModuleRevision moduleRevision = resolver.getDependency(dependencyDescriptor, null);
         ModuleRevisionId resolvedModuleRevisionId = moduleRevision.getDescriptor().getResolvedModuleRevisionId();
